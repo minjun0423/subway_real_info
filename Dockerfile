a892d6efa562be3c30104ded820bb783f3347387
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-06-06 17:07:59 +0900

    test

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,7 @@
-# 베이스 이미지로 Python 3.9 사용
-FROM python:3.9
+FROM python:3.10
 
-# 작업 디렉토리를 설정합니다
-WORKDIR /app
+WORKDIR /code
 
-# 필요한 패키지를 설치합니다
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+COPY ./requirements.txt /code/requirements.txt
 
-# 애플리케이션 소스 코드를 복사합니다
-COPY ./app /app
-
-# Uvicorn을 사용하여 애플리케이션을 실행합니다
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+RUN pip3 install --no-cache-dir --upgrade -r /code/requirements.txt
--- app/main.py
+++ app/main.py
@@ -1,44 +1,36 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
 
-"""
-API_KEY = "427a53796b6d696e37344f6451594a"  # 서울시 오픈API 키
-BASE_URL = "http://swopenapi.seoul.go.kr/api/subway"
-
-class ArrivalInfo(BaseModel):
-    열차번호: str
-    호선: str
-    상하행선구분: str
-    지하철_위치: str
-    지하철_도착까지: str
-"""
+# 서울시 지하철 실시간 도착정보 Open API 키
+API_KEY = 'YOUR_API_KEY'
 
 @app.get("/")
 def read_root():
-    return {"message": "Subway Info"}
+    return {"message": "서울시 지하철 실시간 도착 정보 API. /subway/{station} 경로를 통해 검색하세요."}
 
-"""
 @app.get("/subway/{station}")
-def get_real_time_arrival_info(station: str):
-    url = f"{BASE_URL}/{API_KEY}/json/realtimeStationArrival/0/5/{station}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()  # JSON 형식 변환 데이터
-        
-        processed_data = []  # 데이터 가공
-        for arrival_info in data.get("realtimeArrivalList", []):
-            processed_data.append(ArrivalInfo(
-                열차번호=arrival_info.get("btrainNo", ""),
-                호선=arrival_info.get("subwayId", ""),
-                상하행선구분=arrival_info.get("updnLine", ""),
-                지하철_위치=arrival_info.get("arvlMsg3", ""),
-                지하철_도착까지=arrival_info.get("arvlMsg2", "")
-            ))
-        return processed_data
-    except Exception as e:
-        return {"error": str(e)}
-"""
+def get_subway_info(station: str):
+    url = f"http://swopenAPI.seoul.go.kr/api/subway/{API_KEY}/json/realtimeStationArrival/0/5/{station}"
+    response = requests.get(url)
+    data = response.json()
+
+    if 'errorMessage' in data:
+        raise HTTPException(status_code=404, detail=data['errorMessage']['message'])
+
+    results = []
+    for item in data['realtimeArrivalList']:
+        result = {
+            '열차번호': item['trainNo'],
+            '지하철호선ID': item['subwayId'],
+            '첫번째도착메세지': item['arvlMsg2'],
+            '두번째도착메세지': item['arvlMsg3']
+        }
+        results.append(result)
+
+    return results
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000)
